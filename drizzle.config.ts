c5102c59556960abfbe128e36a83abe25126ab1d
physphile <physphile@vk.com>
2025-05-31 14:40:00 +0300

    wip

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,23 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
-if (!process.env.DATABASE_URL) {
-	throw new Error("DATABASE_URL is not set");
+if (
+	!process.env.DB_NAME ||
+	!process.env.DB_HOST ||
+	!process.env.DB_PASSWORD ||
+	!process.env.DB_PORT ||
+	!process.env.DB_USER
+) {
+	throw new Error("DB_NAME, DB_HOST, DB_PASSWORD, DB_PORT or DB_USER are not set");
 }
 
 export default defineConfig({
 	dbCredentials: {
-		url: process.env.DATABASE_URL,
+		database: process.env.DB_NAME,
+		host: process.env.DB_HOST,
+		password: process.env.DB_PASSWORD,
+		port: Number.parseInt(process.env.DB_PORT),
+		user: process.env.DB_USER,
 	},
 	dialect: "postgresql",
 	out: "./drizzle",
--- src/database/db.ts
+++ src/database/db.ts
@@ -3,8 +3,19 @@ import { drizzle } from "drizzle-orm/bun-sql";
 
 import * as schema from "./schema";
 
-if (!process.env.DATABASE_URL) {
-	throw new Error("DATABASE_URL is not set");
+if (
+	!process.env.DB_HOST ||
+	!process.env.DB_PORT ||
+	!process.env.DB_NAME ||
+	!process.env.DB_USER ||
+	!process.env.DB_PASSWORD
+) {
+	throw new Error("DB_HOST, DB_PORT, DB_NAME, DB_USER or DB_PASSWORD are not set");
 }
 
-export const db = drizzle(process.env.DATABASE_URL, { casing: "snake_case", schema });
+export const db = drizzle(
+	encodeURI(
+		`postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+	),
+	{ casing: "snake_case", schema }
+);
